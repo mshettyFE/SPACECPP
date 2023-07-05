5b71575be2ca0755081275b3301ee3855a8c4f62
Mihir Shetty <ms6556@columbia.edu>
2023-07-05 14:51:32 -0400

    Added MPI to CMAKE

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#SPACECPP
+# SPACECPP
+# Compiling
+Need to specify location of mpiicxx compiler. For NSLS-II, this is located at
+`
+/nsls2/software/ap/mpich/3.3.2-gcc-9.3.0/bin/mpicxx
+`
---
+++ include/Constants.h
@@ -0,0 +1,26 @@
+#ifndef SPACE_CONSTANTS
+#define SPACE_CONSTANTS
+
+#define <vector>
+#define <string>
+
+    int ng1 = 1319;
+    int ng2 = 2;
+    int ng1g2= 2000;
+    int npart=120000;
+    int ngridd=256;
+    int NJ = 400;
+    int NG = 128;
+    int NTY=10;
+    int nup=19;
+    int nup1=19;
+    int nharmh=1320;
+    
+    std::vector<std::string> fnames = {
+        "Absbunchmode0_1.dat",
+        "Absbunchmode2_3.dat",
+        "Fillpattern.dat",
+        "Imbunchmode0_1.dat",
+    }
+
+#endif
